eead803338ed641a34b7e7feebdae6d5a364f0b4
Yannick <yannick.dorn@gmail.com>
2019-05-17 18:35:56 -0400

    made some changes to pathing

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 const port = 3000;
-app.get('/random.text', function(req, res) {
-    res.send('random.txt');
-
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('index.html'));
+});
+app.get('/nav', function(req, res) {
+    res.sendFile(path.resolve('nav.html'));
+});
+app.get('/calendar', function(req, res) {
+    res.sendFile(path.resolve('calendar.html'));
+});
+app.get('/todo', function(req, res) {
+    res.sendFile(path.resolve('todo.html'));
+});
+app.get('/boot-bottom.html', function(req, res) {
+    res.sendFile(path.resolve('boot-bottom.html'));
+});
+app.get('/boot-top.html', function(req, res) {
+    res.sendFile(path.resolve('boot-top.html'));
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
