3952b39dc4626b27286371073640609453a635c4
Yannick <yannick.dorn@gmail.com>
2019-07-16 16:05:26 -0400

    fixed some pathing and asset management

--- server/app.js
+++ server/app.js
@@ -2,26 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 const port = 3000;
+
+
+
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.resolve('index.html'));
-});
-app.get('/nav', function(req, res) {
-    res.sendFile(path.resolve('nav.html'));
-});
-app.get('/calendar', function(req, res) {
-    res.sendFile(path.resolve('calendar.html'));
-});
-app.get('/todo', function(req, res) {
     res.sendFile(path.resolve('todo.html'));
 });
+app.get('/nav', function(req, res) { res.sendFile(path.resolve('nav.html'));
+
+})
 app.get('/boot-bottom.html', function(req, res) {
     res.sendFile(path.resolve('boot-bottom.html'));
 });
 app.get('/boot-top.html', function(req, res) {
     res.sendFile(path.resolve('boot-top.html'));
 });
-app.get('/style', function(req, res) {
-	res.sendFile(path.resolve('style.css'));
-
-})
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
