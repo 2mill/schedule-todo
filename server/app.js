d2f62cc23f87dfaa2f827e459e92beb182fd9ff5
Yannick <yannick.dorn@gmail.com>
2019-07-16 17:42:08 -0400

    Removed check boxes, starting to integrate javascript into the project at this point

--- server/app.js
+++ server/app.js
@@ -8,15 +8,10 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.resolve('todo.html'));
+    res.sendFile(path.resolve('index.html'));
 });
-app.get('/nav', function(req, res) { res.sendFile(path.resolve('nav.html'));
+app.get('/todo', function(req, res) {
+	res.sendFile(path.resolve('todo.html'));
+});
 
-})
-app.get('/boot-bottom.html', function(req, res) {
-    res.sendFile(path.resolve('boot-bottom.html'));
-});
-app.get('/boot-top.html', function(req, res) {
-    res.sendFile(path.resolve('boot-top.html'));
-});
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
