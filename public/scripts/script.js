2d7b7bafa8afa2fe6d5d48f0208c06574969bd0b
root <root@DESKTOP-R4DIHDE.localdomain>
2019-07-17 00:59:58 -0400

    started adding in some jQuery

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+	$('#nav-placeholder').load('/pages/nav.html');
+});
 
+$("#tab-content-box").click(function() {
+	$('#tab-content-box').load('pages/add-row.html');
+});
+$("#tab-content-box").click(function() {
+	alert("This is an alert");
+
+});
+$("#new-tab-button").click(function(){
+  alert("The paragraph was clicked.");
+});
